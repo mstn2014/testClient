3b7aa66d22945a2b6916f99ee96285f33e6ce691
Hashimoto <era88537@gmail.com>
2014-10-27 11:59:05 +0900

    [その他]Androidランキングのシーンセーブ

---
+++ Assets/Hashimoto/Ranking.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ranking : MonoBehaviour {
+
+	UILabel Rank,Id,Name,Score;
+
+	// Use this for initialization
+	void Start () {
+		Rank = this.transform.FindChild("Panel/Rank").GetComponent<UILabel> ();
+		Id	 = this.transform.FindChild("Panel/ID").GetComponent<UILabel> ();
+		Name = this.transform.FindChild("Panel/Name").GetComponent<UILabel> ();
+		Score= this.transform.FindChild("Panel/Score").GetComponent<UILabel> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Rank.text = "1";
+		Id.text = "1893";
+		Name.text = "ABCD";
+		Score.text= "100095";
+	}
+
+}
