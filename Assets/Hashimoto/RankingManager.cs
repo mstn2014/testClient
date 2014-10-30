03d9176cbd38b6cda053eecf6cee221485d80eb1
Hashimoto <era88537@gmail.com>
2014-10-30 13:53:58 +0900

    [追加]ランキングをNGUIで表示する機能(android)

--- Assets/Hashimoto/RankingManager.cs
+++ Assets/Hashimoto/RankingManager.cs
@@ -3,28 +3,46 @@ using System.Collections;
 
 public class RankingManager : MonoBehaviour {
 
-	public GameObject prefab;
+	getRequestAndroid getreq;
 
+	public GameObject prefab;
 	int scaleNum;	
-	float xpos,ypos,diff;
+	float rank_xpos,rank_ypos,diff;
+	int rank_num;
+	GameObject[] rank_save;
 
 	// Use this for initialization
 	void Start () {
+		getreq = GameObject.Find("/getRequestObject").GetComponent<getRequestAndroid>();
+
+		rank_num = 10;
 		scaleNum = 160;
-		xpos = (float)60.0f;
-		ypos = (float)174.0f;
+		rank_xpos = (float)60.0f;
+		rank_ypos = (float)174.0f;
 		diff = (float)36.0f;
-		for(int i=0; i<10; i++){
+		rank_save = new GameObject[rank_num];
+		for(int i=0; i<rank_num; i++){
 			GameObject ranking = (GameObject)GameObject.Instantiate(prefab, this.transform.position, this.transform.rotation );
 			ranking.transform.parent = this.transform;
 			ranking.transform.localScale = new Vector3(scaleNum,scaleNum,scaleNum);
-			ranking.transform.localPosition = new Vector3(xpos,ypos,(float)0.0f);
-			ypos = ypos - diff; 
+			ranking.transform.localPosition = new Vector3(rank_xpos,rank_ypos,(float)0.0f);
+			rank_ypos = rank_ypos - diff; 
+			rank_save[i] = ranking;
 		}
+		//getreq.DataSend();															
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+
+	public void SendDataDisp(int rank, getRequestAndroid.data_android toShow){
+		RankingDisplay rd = rank_save[rank].GetComponent<RankingDisplay>();
+		rd.RankDisplay((rank+1).ToString());
+		rd.IdDisplay(toShow.id.ToString());
+		rd.NameDisplay(toShow.name);
+		rd.ScoreDisplay(toShow.score.ToString());
 	}
 }
---
+++ Assets/Scripts/getRequestAndroid.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using WWWKit;
+using MiniJSON;
+
+public class getRequestAndroid : MonoBehaviour {
+
+	    // サーバから受け取るデータ構造
+    public class data_android : Object
+    {
+        public string name;
+        public long id, score;
+    }
+
+    private MonoBehaviour mMonoBehaviour;
+
+  	WWWClientManager cm;
+    List<data_android> containerList;
+    //public string ipaddr;       // インスペクター上で設定すること
+    int startNum = 0;           // ランキングの初期値
+    data_android[] dt;
+
+    int xpos = 200;
+
+    RankingManager rm;
+
+    // Use this for initialization
+    void Start()
+    {
+        cm = new WWWClientManager(this);
+        containerList = new List<data_android>();
+        getMessage();
+
+        rm = GameObject.Find("/UI Root (2D)/Camera/Anchor/Panel").GetComponent<RankingManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            startNum += 20;
+            if (startNum > containerList.Count) startNum -= 20;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            startNum -= 20;
+            if (startNum < 0) startNum = 0;
+        }
+
+        int count = (containerList.Count - startNum) > 20 ? 20 : (containerList.Count - startNum);
+        dt = new data_android[count];
+        containerList.CopyTo(startNum, dt, 0, count);
+    }
+
+    //--------------------------------------------------------
+    // UIButtonMessageからのメッセージを受ける関数
+    //--------------------------------------------------------
+    void getMessage()
+    {
+        string url = "http://mstn2014-osaka.herokuapp.com/users/score";
+        cm.GET(url, "ReceiveRequest");
+    }
+
+    void ReceiveRequest(WWW www)
+    {
+        string json = www.text;
+        var scoreInfo = Json.Deserialize(json) as Dictionary<string, object>;
+        int i = 1;
+        containerList.Clear();
+        foreach (object ob in scoreInfo)
+        {
+            Dictionary<string, object> num = (Dictionary<string, object>)scoreInfo[i.ToString()];
+            long id = (long)num["id"];
+            string name = (string)num["name"];
+            long score = (long)num["score"];
+            data_android data1 = new data_android();
+            data1.id = id;
+            data1.name = name;
+            data1.score = score;
+            containerList.Add(data1);
+            i++;
+        }
+    }
+
+    void ReceiveError()
+    {
+        Debug.Log("GET出来ませんでした。");
+    }
+
+    //--------------------------------------------------------
+    // ランキングの描画
+    //--------------------------------------------------------
+    void OnGUI()
+    {
+        /*GUI.Label(new Rect(xpos, 0, 100, 100), "rank");
+        GUI.Label(new Rect(xpos + 30, 0, 100, 100), "id");
+        GUI.Label(new Rect(xpos + 60, 0, 100, 100), "name");
+        GUI.Label(new Rect(xpos + 120, 0, 100, 100), "score");*/
+        drawTable();
+    }
+
+    private void drawsingleline(int pos, data_android toShow)
+    {
+       /* pos++;
+        GUI.Label(new Rect(xpos, pos * 20, 100, 100), (startNum + pos).ToString());
+        GUI.Label(new Rect(xpos + 30, pos * 20, 100, 100), toShow.id.ToString());
+        GUI.Label(new Rect(xpos + 60, pos * 20, 100, 100), toShow.name);
+        GUI.Label(new Rect(xpos + 120, pos * 20, 100, 100), toShow.score.ToString());*/
+    }
+
+    private void drawTable()
+    {
+       if (containerList.Count == 0) return;
+
+        int j = 0;
+        foreach (data_android thecont in dt)
+        {
+            //drawsingleline(j, thecont);
+            rm.SendDataDisp(j,thecont);
+            j++;
+            if(j == 10) return;
+        }
+    }
+}
